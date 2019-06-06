da30e325f958425d2956df6bb178cb85d86d4197
bugszhou <bugszhou@outlook.com>
2019-06-06 10:28:22 +0800

    fix(watch more):

--- template/scripts/lib/gulp/index.js
+++ template/scripts/lib/gulp/index.js
@@ -5,16 +5,16 @@ const gulp = require('gulp'),
   conf = require('../../etc'),
   codePath = conf[process.env.PRJ_ENV].codePath,
   entryHash = require('../utils/entryHash'),
-  {getEntry} = require('miniapp-auto-webpack-plugin');
+  { getEntry } = require('miniapp-auto-webpack-plugin');
 
 
-module.exports = function({compiler, watchFn}) {
+module.exports = function({ compiler, watchFn }) {
   const scope = this;
   if (!watchFn) {
     throw new Error('Compiler not start;');
   }
   scope.webpackWatcher = watchFn();
-  let {entry} = getEntry({
+  let { entry } = getEntry({
       xmlSuffix: conf.xmlSuffix,
       cssSuffix: conf.cssSuffix,
       compileCssSuffix: conf.compileCssSuffix,
@@ -24,43 +24,6 @@ module.exports = function({compiler, watchFn}) {
   scope.jsonWatcher.on('change', reWatch);
   scope.jsonWatcher.on('add', reWatch);
   scope.jsonWatcher.on('unlink', reWatch);
-  scope.allWatcher = gulp.watch('src/**/*', {
-    events: ['add', 'addDir', 'unlinkDir', 'unlink'],
-    ignored: new RegExp(`\.(js|json|${conf.xmlSuffix}${conf.miniJsSuffix ? `|${conf.miniJsSuffix}` : ''}|${conf.compileCssSuffix})$`)
-  });
-
-  let hasRunTimer = false,
-    preLen = 0,
-    curLen = 0;
-  scope.allWatcher.on('all', function() {
-    ++curLen;
-    if (!hasRunTimer) {
-      hasRunTimer = true;
-      checkWatcherEnd()
-    }
-  });
-
-  function checkWatcherEnd() {
-    setTimeout(() => {
-      if (curLen === preLen) {
-        hasRunTimer = false;
-        if (scope.webpackWatcher) {
-          cpProjectConfig();
-          scope.webpackWatcher.close(() => {
-            console.log('Assets File changed!');
-            setTimeout(() => {
-              scope.webpackWatcher = watchFn();
-            }, 200);
-            preLen = curLen = 0;
-          });
-          scope.webpackWatcher = null;
-          return false;
-        }
-      }
-      preLen = curLen;
-      checkWatcherEnd();
-    }, 300);
-  }
 
   function reWatch() {
     let curHashVal = entryHash(getEntry({
@@ -75,7 +38,9 @@ module.exports = function({compiler, watchFn}) {
       cpProjectConfig();
       scope.webpackWatcher.close(() => {
         console.log('Entry File changed!');
-        scope.webpackWatcher = watchFn();
+        setTimeout(() => {
+          scope.webpackWatcher = watchFn();
+        }, 100);
       });
       scope.webpackWatcher = null;
     }
