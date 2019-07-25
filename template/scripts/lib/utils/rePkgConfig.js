ff4ebaa4d6c83bcea5f6622a6bd4a433ce7d69d6
bugszhou <bugszhou@outlook.com>
2019-07-25 16:32:19 +0800

    feat: Use appid from yaml

--- template/scripts/lib/utils/rePkgConfig.js
+++ template/scripts/lib/utils/rePkgConfig.js
@@ -1,7 +1,15 @@
 const merge = require('merge'),
-  {prjEnvComp} = require('./envCompare'),
+  { prjEnvComp } = require('./envCompare'),
+  envData = require('../envData/getEnvData'),
   distPkg = require('../../etc/project.config');
 module.exports = function(content = '{}') {
+  let appid = '';
+  try {
+    appid = JSON.parse(envData || '{}').APP_ID || '';
+    content = JSON.stringify(merge(JSON.parse(content), appid ? { appid } : ''));
+  } catch (e) {
+    console.error(e);
+  }
   if (prjEnvComp('development')) {
     return content;
   }
